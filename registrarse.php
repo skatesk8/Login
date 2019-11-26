7d32e1b761e0301b8e92b5549aeca918ba597192
dvasquez_fc <dvasquez.wc@gmail.com>
2019-11-26 17:21:08 -0600

    Creacion del registro.php

---
+++ registrarse.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>DAI-BIT</title>
+    <link rel="stylesheet" href="css/style.css">
+
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+</head>
+
+<body>
+
+    <div class="container-all">
+        <div class="ctn-text">
+            <div class="capa"></div>
+            <h1 class="title-description">Lorem ipsum dolor sit amet.</h1>
+            <p class="text-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat natus dolore ea dicta molestias totam iusto perspiciatis iste obcaecati quod dolor, eaque qui asperiores? Molestias ad nemo placeat est quidem?</p>
+        </div>
+
+        <div class="ctn-form">
+            <img src="images/logo-magtimus-small.png" alt="" class="logo">
+            <h1 class="title">Registrarse</h1>
+
+            <form action="" method="post">
+               
+                <label for="">Nombre de Usuario</label>
+                <input type="text" name="username">
+                <span class="msg-error"></span>
+                <label for="">Email</label>
+                <input type="text" name="email">
+                <span class="msg-error"></span>
+                <label for="">Contraseña</label>
+                <input type="password" name="password">
+                <span class="msg-error"></span>
+
+                <input type="submit" value="Registrarse">
+
+            </form>
+
+            <span class="text-footer">¿Ya te has registrado?
+                <a href="Index.php">Iniciar Sesión</a>
+            </span>
+        </div>
+
+    </div>
+
+</body>
+
+</html>
